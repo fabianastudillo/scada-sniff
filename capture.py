07432f81ad3ace36789a1847ec0b339dd93c5ed6
ClaudiaPadilla <160961709+ClaudiaPadilla@users.noreply.github.com>
2024-04-10 15:37:00 -0500

    Update capture.py

--- capture.py
+++ capture.py
@@ -32,11 +32,11 @@ def manejar_paquete(packet):
     if mb.ModbusADUResponse in packet:
 
         #logger.debug("ADUResponse IP.src={packet[IP].src} IP.dst={packet[IP].dst}")
-        logger.debug("ADUResponse IP.src=%s IP.dst=%s Identifier=%s", packet[IP].src, packet[IP].dst,identifier)
+        logger.debug("ADUResponse IP.src=%s IP.dst=%s , packet[IP].src, packet[IP].dst)
         #packet.show()
     elif mb.ModbusADURequest in packet:
         #logger.debug("ADURequest IP.src={packet[IP].src} IP.dst={packet[IP].dst}")
-        logger.debug("ADURequest IP.src=%s IP.dst=%s Identifier=%s", packet[IP].src, packet[IP].dst,identifier)
+        logger.debug("ADURequest IP.src=%s IP.dst=%s , packet[IP].src, packet[IP].dst)
         #packet.show()
 
 # Set logs
