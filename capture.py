b6c4874135170550e225a166497f12018d3fc044
ClaudiaPadilla <160961709+ClaudiaPadilla@users.noreply.github.com>
2024-04-10 15:08:44 -0500

    Update capture.py

--- capture.py
+++ capture.py
@@ -16,13 +16,27 @@ def manejar_paquete(packet):
     #if len(packet[TCP].payload):
     #    datos = packet[TCP].payload.load
     #    print("Datos del paquete (puede incluir cabecera Modbus):", datos)
+    if packet[IP].src == '192.168.222.9':
+        identifier = "APIS1"
+    elif packet[IP].src == '192.168.222.15':
+        identifier = "APIS3"
+    elif packet[IP].src == '192.168.222.14':
+        identifier = "APIS2"
+    elif packet[IP].src == '192.168.222.12':
+        identifier = "APIS2"
+    elif packet[IP].src == '192.168.222.11':
+        identifier = "APIS2"
+    elif packet[IP].src == '192.168.222.13':
+        identifier = "APIS2"
+
     if mb.ModbusADUResponse in packet:
+
         #logger.debug("ADUResponse IP.src={packet[IP].src} IP.dst={packet[IP].dst}")
-        logger.debug("ADUResponse IP.src=%s IP.dst=%s", packet[IP].src, packet[IP].dst)
+        logger.debug("ADUResponse IP.src=%s IP.dst=%s Identifier=%s", packet[IP].src, packet[IP].dst,identifier)
         #packet.show()
     elif mb.ModbusADURequest in packet:
         #logger.debug("ADURequest IP.src={packet[IP].src} IP.dst={packet[IP].dst}")
-        logger.debug("ADURequest IP.src=%s IP.dst=%s", packet[IP].src, packet[IP].dst)
+        logger.debug("ADURequest IP.src=%s IP.dst=%s Identifier=%s", packet[IP].src, packet[IP].dst,identifier)
         #packet.show()
 
 # Set logs
